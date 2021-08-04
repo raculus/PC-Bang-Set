2d4c3d3bcf62d10b24794e28248bc3d6f2b34757
DESKTOP-65P7IVV\muhjh <muhjh6@gmail.com>
2021-08-04 20:10:54 +0900

    uploads

---
+++ Game-Set/Form_Main.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Game_Set
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button_Apply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "오버워치 그래픽 설정",
+            "포인터 정확도 끄기",
+            "지포스 드라이버 다운로드",
+            "불필요 프로세스 끄기"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 13);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(213, 204);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // button_Apply
+            // 
+            this.button_Apply.Location = new System.Drawing.Point(12, 228);
+            this.button_Apply.Name = "button_Apply";
+            this.button_Apply.Size = new System.Drawing.Size(213, 37);
+            this.button_Apply.TabIndex = 1;
+            this.button_Apply.Text = "적용";
+            this.button_Apply.UseVisualStyleBackColor = true;
+            this.button_Apply.Click += new System.EventHandler(this.button_Apply_Click);
+            // 
+            // Form_Main
+            // 
+            this.AcceptButton = this.button_Apply;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 277);
+            this.Controls.Add(this.button_Apply);
+            this.Controls.Add(this.checkedListBox1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_Main";
+            this.Text = "PCBang-Set";
+            this.Load += new System.EventHandler(this.Form_Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button_Apply;
+    }
+}
+
---
+++ Game-Set/Form_Main.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Set
+{
+    public partial class Form_Main : Form
+    {
+        [Flags]
+        public enum SPIF
+        {
+            None = 0x00,
+            /// <summary>Writes the new system-wide parameter setting to the user profile.</summary>
+            SPIF_UPDATEINIFILE = 0x01,
+            /// <summary>Broadcasts the WM_SETTINGCHANGE message after updating the user profile.</summary>
+            SPIF_SENDCHANGE = 0x02,
+            /// <summary>Same as SPIF_SENDCHANGE.</summary>
+            SPIF_SENDWININICHANGE = 0x02
+        }
+
+        // http://stackoverflow.com/questions/24737775/toggle-enhance-pointer-precision
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoGet(uint action, uint param, IntPtr vparam, SPIF fWinIni);
+        [DllImport("user32.dll", EntryPoint = "SystemParametersInfo", SetLastError = true)]
+        public static extern bool SystemParametersInfoSet(uint action, uint param, IntPtr vparam, SPIF fWinIni);
+
+        public const UInt32 SPI_GETMOUSE = 0x0003;
+        public const UInt32 SPI_SETMOUSE = 0x0004;
+
+        public static bool PointerAccel(bool b)
+        {
+            int[] mouseParams = new int[3];
+            // Get the current values.
+            SystemParametersInfoGet(SPI_GETMOUSE, 0, GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), 0);
+            // Modify the acceleration value as directed.
+            mouseParams[2] = b ? 1 : 0;
+            // Update the system setting.
+            return SystemParametersInfoSet(SPI_SETMOUSE, 0, GCHandle.Alloc(mouseParams, GCHandleType.Pinned).AddrOfPinnedObject(), SPIF.SPIF_SENDCHANGE);
+        }
+
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+        private void Form_Main_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+        }
+        private string krokr(string sub)
+        {
+            string url = @"http://";
+            url += sub + ".kotlin.kro.kr";
+            return url;
+        }
+
+        private void apply_ow_setting()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            path += @"\Overwatch\Settings\Settings_v0.ini";
+            Downloader(krokr("ow-setting"), path);
+        }
+
+        private void Downloader(string url, string path)
+        {
+            using (var client = new WebClient())
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                client.DownloadFile(url, path);
+            }
+        }
+        private void kill_useless_process()
+        {
+            string useless = "MainTitleFor2Dx,LCore,POOQLauncher,PWLauncher,LuniparkAgent,마스크";
+            foreach(var name in useless.Split(','))
+            {
+                Process[] processList = Process.GetProcessesByName(name);
+                if(processList.Length > 0)
+                    processList[0].Kill();
+            }
+        }
+        private void gpu_driver()
+        {
+            string url = krokr("gpu");
+            try
+            {
+                Process.Start("chrome \"" + url + "\"");
+            }
+            catch
+            {
+                Process.Start(url);
+            }
+        }
+        private void button_Apply_Click(object sender, EventArgs e)
+        {
+            foreach(string checkedItem in checkedListBox1.CheckedItems)
+            {
+                int index = checkedListBox1.Items.IndexOf(checkedItem);
+                if(index == 0)
+                {
+                    //오버워치 그래픽 설정
+                    apply_ow_setting();
+                }
+                else if(index == 1)
+                {
+                    //포인터 정확도 끄기
+                    PointerAccel(false);
+                }
+                else if(index == 2)
+                {
+                    //지포스 드라이버 다운로드
+                    gpu_driver();
+                }
+                else if(index == 3)
+                {
+                    //불필요 프로세스 끄기
+                    kill_useless_process();
+                }
+            }
+        }
+    }
+}
